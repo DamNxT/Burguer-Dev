e825e60ae1636f7571e6b5067869a91da8ce6116
Danilo <DaniloSilva_ds@outlook.com>
2024-03-12 22:57:54 -0300

    add funcionalidade p/ carrinho e items

---
+++ script.js
@@ -0,0 +1,30 @@
+const menu = document.getElementById("menu")
+const cartBtn = document.getElementById("cart-btn")
+const cartModal = document.getElementById("cart-modal")
+const cartItemsContainer = document.getElementById("cart-items")
+const cartTotal = document.getElementById("cart-total")
+const checkoutBtn = document.getElementById("checkout-btn")
+const closeModalBtn = document.getElementById("close-modal-btn")
+const cartCounter = document.getElementById("cart-count")
+const addressInput = document.getElementById("address")
+const addressWarn = document.getElementById("address-warn")
+
+//Abrir o modal do carrinho
+cartBtn.addEventListener("click", function() {
+    cartModal.style.display = "flex"
+})
+
+//Fechar o modal do carrinho clicando fora
+cartModal.addEventListener("click", function(event){
+    if (event.target === cartModal){
+        cartModal.style.display = "none"
+    }
+})
+//fechar através do botão
+closeModalBtn.addEventListener("click", function() {   
+        cartModal.style.display = "none"
+})
+
+menu.addEventListener("click", function(event){
+    let parentButton = event.target.closest(".add-to-cart-btn")
+})
