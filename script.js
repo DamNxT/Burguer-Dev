afabc33a32d103c39e527e7b1560138869e4939d
Danilo <DaniloSilva_ds@outlook.com>
2024-03-13 21:53:51 -0300

    Alt final

--- script.js
+++ script.js
@@ -171,7 +171,7 @@ checkoutBtn.addEventListener("click", function(){
 function checkRestaurantOpen(){
     const data = new Date();
     const hora = data.getHours();
-    return hora >= 22 && hora < 23;
+    return hora >= 18 && hora < 22;
 }
 
 const spanItem = document.getElementById("date-span")
